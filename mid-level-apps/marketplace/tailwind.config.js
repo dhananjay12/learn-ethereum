4b388ec8c2ff162d9f71a351744947edb82617cf
dhananjay12 <dhananjay1204@gmail.com>
2022-02-18 22:36:35 +0100

    componenets

--- mid-level-apps/marketplace/tailwind.config.js
+++ mid-level-apps/marketplace/tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      maxWidth: {
+        "8xl": "1920px"
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
