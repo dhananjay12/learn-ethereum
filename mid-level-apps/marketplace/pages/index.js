d2a382620c9ff9b049eb852e18c32ea794c6703f
dhananjay12 <dhananjay1204@gmail.com>
2022-02-19 11:43:58 +0100

    UI changes

--- mid-level-apps/marketplace/pages/index.js
+++ mid-level-apps/marketplace/pages/index.js
@@ -1,26 +1,27 @@
 
-import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common"
-import { CourseList } from "@components/course"
-import { OrderCard } from "@components/order"
-import { EthRates, WalletBar } from "@components/web3"
+import { Hero } from "@components/ui/common"
+import { CourseList } from "@components/ui/course"
+import { BaseLayout } from "@components/ui/layout"
+import { getAllCourses } from "@content/courses/fetcher"
 
-export default function Home() {
+export default function Home({courses}) {
   return (
-    <div>
-      <div className="relative bg-white overflow-hidden">
-        <div className="relative max-w-7xl mx-auto px-4">
-          <Navbar />
-          <div className="fit">
-            <Hero />
-            <Breadcrumbs />
-            <WalletBar />
-            <EthRates />
-            <OrderCard />
-            <CourseList />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </div>
+    <>
+      <Hero />
+      <CourseList
+        courses={courses}
+      />
+    </>
   )
 }
+
+export function getStaticProps() {
+  const { data } = getAllCourses()
+  return {
+    props: {
+      courses: data
+    }
+  }
+}
+
+Home.Layout = BaseLayout
