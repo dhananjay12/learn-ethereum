de656fd2c34e3e1d982d6b0eadfb473367181c6a
dhananjay12 <dhananjay1204@gmail.com>
2022-02-20 12:02:19 +0100

    hooks

--- mid-level-apps/marketplace/components/providers/web3/hooks/setupHooks.js
+++ mid-level-apps/marketplace/components/providers/web3/hooks/setupHooks.js
@@ -1,13 +1,12 @@
-import { useAccount } from "./useAccount";
+import { handler as createUseAccount } from "./useAccount";
 
 const DEFAULT_HOOKS = {
   useAccount: () => ({account: null})
 }
 
 export const setupHooks = web3 => {
-  if (!web3) { return DEFAULT_HOOKS }
 
   return {
-    useAccount: useAccount(web3)
+    useAccount: createUseAccount(web3)
   }
 }
